321caa4e7ce0d67f189da143b511a7067d20ee09
NoamK <noamklopper@gmail.com>
2020-08-21 01:31:12 +0300

    Platform work and Parrot import!!

---
+++ Assets/Art/Parrot/readMe.txt
@@ -0,0 +1,27 @@
+<strong>Parrot</strong> is coming! Diffuse+Normal+Spec with 13 cool animations.
+Made for your mmo/rpg/fps/action game! Great choice for high quality visual!
+
+<strong>Model</strong>
+Parrot 3250 Tris
+
+<strong>Textures</strong>
+skinbody_d 1024x1024
+skinbody_n 1024x1024
+skinbody_sp 512x512
+
+<strong>Animations</strong>
+Atk
+Die
+Die2
+Die3
+Idle 1
+Idle 2
+Idle 3
+Idle 4
+Idle 5
+Land
+Run
+Take off
+Walk
+
+Please rate us if you like! We will bring you more!
---
+++ Assets/Scripts/PlatformStay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformStay : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject thePlatform;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            print("onplatform");
+
+            Player.transform.parent = thePlatform.transform;
+            // Player.transform.SetParent(gameObject.transform);
+            print("ou");
+            // }
+
+        }
+    }
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject == Player)
+            {
+
+                Player.transform.parent = null;
+
+            }
+        }
+    } 
+
+
