610f88f260d0ccd1064675eb67604d489cd63cdc
Jonassa5 <69770108+Jonassa5@users.noreply.github.com>
2020-08-23 12:43:00 +0300

    body

---
+++ Assets/Scripts/KeyPickUp.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPickUp : MonoBehaviour
+{
+    public Transform playerKeyHolder;
+    public Transform jailDoorKeyHolder;
+    private bool tookKeyOnce= false;
+    public GameObject jailDoor;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player" && tookKeyOnce == false)  
+        {
+            tookKeyOnce = true;
+            transform.parent = playerKeyHolder.transform;
+            transform.position = playerKeyHolder.position;//new Vector3(playerTransform.x, playerTransform.position.y - 6f, playerTransform.position.z);
+            
+            print ("gotcha!");
+        }
+        if (collider.gameObject.tag == "JAILDOOR")
+        {
+           
+            tookKeyOnce = true;
+            transform.parent = jailDoorKeyHolder.transform;
+            transform.position = jailDoorKeyHolder.position;
+            transform.rotation = jailDoorKeyHolder.rotation;
+            jailDoor.GetComponent<Transform>();
+            ///jailDoor.  rotate on hinge 90
+            //jailDoor.transform.Rotate(0,78,0);
+            
+
+        }
+        
+        
+
+    }
+}
---
+++ Assets/Scripts/PlatformStay.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformStay : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject thePlatform;
+    public bool onBeam = false;
+   
+
+  
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // if (other.gameObject.tag == "Player")
+
+        //if (other.gameObject.name == "CarmelPlayer")
+        if (other.gameObject.name == "Moving platform")
+        {
+            print("onplatform");
+            onBeam = true;
+            Player.transform.parent = thePlatform.transform;
+           
+            
+        }
+    }
+   private void OnTriggerExit(Collider other)
+    {
+        onBeam = false;
+        // if (other.gameObject.tag == "Player")
+        // if (other.gameObject.name == "CarmelPlayer")
+      /*  if (other.gameObject.name == "Moving platform")
+        {*/
+            print("off of platform");
+
+            //Player.transform.parent = null;
+            transform.parent = null;
+
+        }
+    }
+
+
+
